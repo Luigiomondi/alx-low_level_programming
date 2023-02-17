721ecb9d4130f190c473c64dae32823c4317d8cd
Luigiomondi <omondiluigi@gmail.com>
2023-02-16 16:55:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
