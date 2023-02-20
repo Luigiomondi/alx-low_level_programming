8f9f6d7e263599d992cc995a71a19951832ba959
Luigiomondi <omondiluigi@gmail.com>
2023-02-20 15:33:23 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+#include	<stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+	*	main	-	Entry	point
+	*
+	*	Return:	Always	0	(Success)
+	*/
 
-int main(void)
+int	main(void)
 {
-    char hex[] = "0123456789abcdef"; /* Array of hexadecimal characters */
-    int i;
+		char	hex[]	=	"0123456789abcdef";	/*	Array	of	hexadecimal	characters	*/
+		int	i;
 
-    for (i = 0; i < 16; i++) /* Loop through array and print each character */
-    {
-        putchar(hex[i]);
-    }
-    putchar('\n'); /* Print a newline character */
+		for	(i	=	0;	i	<	16;	i++)	/*	Loop	through	array	and	print	each	character	*/
+		{
+				putchar(hex[i]);
+		}
+		putchar('\n');	/*	Print	a	newline	character	*/
 
-    return (0);
+		return	(0);
 }
