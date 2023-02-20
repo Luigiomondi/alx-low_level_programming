c455a0de7f9b9a59731b1745f1b7de0c124e2490
Luigiomondi <omondiluigi@gmail.com>
2023-02-20 13:50:39 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,9 +13,10 @@ int main(void)
 	while (c <= 'z')
 	{
 		if (( c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
+		{
+			putchar(c);
 			c++;
 		}
-	putchar('\n');
+		putchar('\n');
 	return (0);
 }
